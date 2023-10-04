792fa0e797d67cc510bd302818642a3b3e939c07
Rowan Marquez <146883179+rnmarquez@users.noreply.github.com>
2023-10-03 17:59:17 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+#This program will introduce myself to my ASTR-19 class.
+#This tells the computer to print my introduction.
+def printintro():
+	print("Hello! My name is Rowan Marquez and my pronouns are she/her. I love to watch the original Star Wars trilogy and my favorite character is Princess Leia. My favorite food is pasta, but I also love mac and cheese.")
+#This defines the main function of the program
+def main():
+	printintro()
+#When we run the program this executes first
+if __name__=="__main__":
+	main()
