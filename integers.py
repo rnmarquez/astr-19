b9db2b4b91ce7481c9c7b8dd36bc092a53be6031
Rowan Marquez <146883179+rnmarquez@users.noreply.github.com>
2023-10-30 14:23:21 -0700

    Add files via upload

---
+++ integers.py
@@ -0,0 +1,16 @@
+
+def mathcode():
+	x = 4.67
+	y = 12.24
+	print("x+y = ", x+y)
+	a = 8
+	b = 3
+	print("a-b = ", a-b)
+	c = 3.14
+	d = 7
+	print("c*d = ", c*d)
+def main():
+	mathcode()
+if __name__=="__main__":
+	main()
+
